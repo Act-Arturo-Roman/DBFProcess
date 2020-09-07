10f12e4aa7a08e6e721d0cee5e2f823cfb755728
Act-Arturo-Roman <actuario.arturo@hotmail.com>
2020-09-06 20:11:52 -0700

    Taller

--- DBF Process/Form1.Designer.cs
+++ DBF Process/Form1.Designer.cs
@@ -34,13 +34,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -54,11 +60,11 @@
             this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.button2, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -68,7 +74,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(709, 316);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(770, 349);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -76,9 +82,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Location = new System.Drawing.Point(3, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 26);
+            this.label1.Size = new System.Drawing.Size(177, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Archivo DBF:";
             // 
@@ -86,19 +92,19 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(172, 16);
+            this.textBox1.Location = new System.Drawing.Point(186, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(402, 30);
+            this.textBox1.Size = new System.Drawing.Size(437, 30);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(587, 12);
+            this.button1.Location = new System.Drawing.Point(636, 15);
             this.button1.Margin = new System.Windows.Forms.Padding(10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 39);
+            this.button1.Size = new System.Drawing.Size(124, 39);
             this.button1.TabIndex = 2;
             this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
@@ -108,10 +114,10 @@
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(10, 262);
+            this.button2.Location = new System.Drawing.Point(10, 295);
             this.button2.Margin = new System.Windows.Forms.Padding(10);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 44);
+            this.button2.Size = new System.Drawing.Size(163, 44);
             this.button2.TabIndex = 3;
             this.button2.Text = "Partir";
             this.button2.UseVisualStyleBackColor = true;
@@ -122,29 +128,20 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 81);
+            this.label2.Location = new System.Drawing.Point(3, 90);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(163, 26);
+            this.label2.Size = new System.Drawing.Size(177, 26);
             this.label2.TabIndex = 4;
             this.label2.Text = "Campo Fecha:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(172, 79);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(181, 30);
-            this.textBox2.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 144);
+            this.label3.Location = new System.Drawing.Point(3, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(163, 26);
+            this.label3.Size = new System.Drawing.Size(177, 26);
             this.label3.TabIndex = 6;
             this.label3.Text = "Formato:";
             // 
@@ -155,8 +152,9 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Fecha",
-            "mm/dd/aaaa"});
-            this.comboBox1.Location = new System.Drawing.Point(172, 141);
+            "mm/dd/aaaa",
+            "dd/mm/aaaa"});
+            this.comboBox1.Location = new System.Drawing.Point(186, 156);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(210, 33);
             this.comboBox1.TabIndex = 7;
@@ -166,9 +164,9 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(3, 207);
+            this.label4.Location = new System.Drawing.Point(3, 228);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(163, 26);
+            this.label4.Size = new System.Drawing.Size(177, 26);
             this.label4.TabIndex = 8;
             this.label4.Text = "Nombre:";
             // 
@@ -176,11 +174,71 @@
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(172, 205);
+            this.textBox3.Location = new System.Drawing.Point(186, 226);
             this.textBox3.MaxLength = 5;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(210, 30);
             this.textBox3.TabIndex = 9;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(186, 72);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(437, 63);
+            this.tableLayoutPanel2.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(221, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 57);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipos";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(121, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Otros";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(87, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fechas";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(3, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 30);
+            this.textBox2.TabIndex = 5;
             // 
             // openFileDialog1
             // 
@@ -191,12 +249,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 316);
+            this.ClientSize = new System.Drawing.Size(770, 349);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Paso1";
             this.Text = "Paso 1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -215,6 +277,10 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
 
--- DBF Process/Form1.cs
+++ DBF Process/Form1.cs
@@ -19,7 +19,8 @@ namespace DBF_Process
         public static int WeeksInYear(DateTime date)
         {
             GregorianCalendar cal = new GregorianCalendar(GregorianCalendarTypes.Localized);
-            return cal.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
+            return cal.GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+            ;
         }
         public class Field
         {
@@ -34,18 +35,29 @@ namespace DBF_Process
         
         public Dbf dbf = new Dbf();
         public DataSet dbfDS = new DataSet();
-        
-        
-        private void button1_Click(object sender, EventArgs e)
+        private string pruebaCampos(string campo)
         {
-            this.openFileDialog1.ShowDialog();
+            DataRow row = dbfDS.Tables["TablaDatos"].Rows[0];
+            for (int i = 1; i <= 50; i++)
+            {
+                try
+                {
+                    if(row[campo + "*" + i.ToString()].GetType().ToString() == "System.String")
+                    {
+                        campo = campo + "*" + i.ToString();
+                        break;
+                    }
+                }
+                catch
+                { }
+            }
+            return campo;
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void procesaFechas()
         {
             Encoding iso = Encoding.GetEncoding("ISO-8859-1");
             Encoding utf8 = Encoding.Unicode;
-            FileInfo fi = new FileInfo(this.textBox1.Text); 
+            FileInfo fi = new FileInfo(this.textBox1.Text);
             DataTable dt = ParseDBF.ReadDBF(this.textBox1.Text);
             dt.TableName = "TablaDatos"; //Nombra el data table
             dbfDS.Tables.Add(dt); //Agrega el data table al dataset
@@ -77,12 +89,12 @@ namespace DBF_Process
                     semanas.Add(tempdate.Year.ToString() + WeeksInYear(tempdate).ToString("00"));
                 }
             }
-            else if(formato == "mm/dd/aaaa")
+            else if (formato == "mm/dd/aaaa")
             {
                 campo = campo + "*10";
                 foreach (DataRow row in dbfDS.Tables["TablaDatos"].Rows)
                 {
-                    day = row[campo].ToString().Substring(3,2);
+                    day = row[campo].ToString().Substring(3, 2);
                     month = row[campo].ToString().Substring(0, 2);
                     year = row[campo].ToString().Substring(6, 4);
                     DateTime tempdate;
@@ -90,9 +102,9 @@ namespace DBF_Process
                     {
                         tempdate = new DateTime(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
                     }
-                    catch 
+                    catch
                     {
-                        MessageBox.Show("!Hay fwchas con formato erroneo");
+                        MessageBox.Show("!Hay fechas con formato erroneo");
                         tempdate = new DateTime(2020, 1, 1);
                     }
                     row["MASTERDATE*8"] = tempdate;
@@ -100,19 +112,43 @@ namespace DBF_Process
                     semanas.Add(tempdate.Year.ToString() + WeeksInYear(tempdate).ToString("00"));
                 }
             }
-           
+            else if (formato == "dd/mm/aaaa")
+            {
+                campo = campo + "*10";
+                foreach (DataRow row in dbfDS.Tables["TablaDatos"].Rows)
+                {
+                    day = row[campo].ToString().Substring(0, 2);
+                    month = row[campo].ToString().Substring(3, 2);
+                    year = row[campo].ToString().Substring(6, 4);
+                    DateTime tempdate;
+                    try
+                    {
+                        tempdate = new DateTime(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
+                    }
+                    catch
+                    {
+                        MessageBox.Show("!Hay fechas con formato erroneo");
+                        tempdate = new DateTime(2020, 1, 1);
+                    }
+                    row["MASTERDATE*8"] = tempdate;
+                    row["YEARNWEEK*6"] = tempdate.Year.ToString() + WeeksInYear(tempdate).ToString("00");
+                    semanas.Add(tempdate.Year.ToString() + WeeksInYear(tempdate).ToString("00"));
+                }
+            }
+
+
             List<Field> DSFields = new List<Field>();
-            foreach(DataColumn Column in dt.Columns)
+            foreach (DataColumn Column in dt.Columns)
             {
                 Field field = new Field();
-                field.FieldName = Column.ColumnName.Substring(0,Column.ColumnName.IndexOf("*", 0));
+                field.FieldName = Column.ColumnName.Substring(0, Column.ColumnName.IndexOf("*", 0));
                 field.FieldType = Column.DataType.ToString();
                 int location = Column.ColumnName.Length - Column.ColumnName.IndexOf("*", 0) - 1;
                 field.FieldLength = Int32.Parse(Column.ColumnName.Substring(Column.ColumnName.IndexOf("*", 0) + 1, location));
                 DSFields.Add(field);
             }
 
-            foreach (Field field in DSFields) 
+            foreach (Field field in DSFields)
             {
                 if (field.FieldType == "System.String")
                 {
@@ -131,13 +167,13 @@ namespace DBF_Process
                     dbf.Fields.Add(new DbfField(field.FieldName, DbfFieldType.Character, Convert.ToByte(field.FieldLength)));
                 }
             }
-            foreach(string semana in semanas)
+            foreach (string semana in semanas)
             {
                 var query = from r in dt.AsEnumerable()
                             where r.Field<string>("YEARNWEEK*6") == semana//Convert.ToInt32(Folio)
                             select r;
                 DataTable dtDestino = query.CopyToDataTable<DataRow>();
-  
+
                 DbfRecord record;
                 foreach (DataRow row in dtDestino.Rows)
                 {
@@ -155,17 +191,138 @@ namespace DBF_Process
                     }
 
                 }
-                dbf.Write(fi.DirectoryName + "//" + textBox3.Text + "S" + semana.Substring(4,2) + ".dbf", DbfVersion.FoxBaseDBase3NoMemo);
+                dbf.Write(fi.DirectoryName + "//" + textBox3.Text + "S" + semana.Substring(4, 2) + ".dbf", DbfVersion.FoxBaseDBase3NoMemo);
                 dbf.Records.Clear();
 
             }
 
             MessageBox.Show("Proceso concluido exitosamente");
+
+        }
+        private void procesaOtros()
+        {
+            Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+            Encoding utf8 = Encoding.Unicode;
+            FileInfo fi = new FileInfo(this.textBox1.Text);
+            DataTable dt = ParseDBF.ReadDBF(this.textBox1.Text);
+            dt.TableName = "TablaDatos"; //Nombra el data table
+            dbfDS.Tables.Add(dt); //Agrega el data table al dataset
+            string campo = textBox2.Text;
+            HashSet<string> uniqueList = new HashSet<string>();
+
+            string campo2 = pruebaCampos(campo);
+            foreach (DataRow row in dbfDS.Tables["TablaDatos"].Rows)
+            {
+                uniqueList.Add(row[campo2].ToString());
+            }
+            List<Field> DSFields = new List<Field>();
+            foreach (DataColumn Column in dt.Columns)
+            {
+                Field field = new Field();
+                field.FieldName = Column.ColumnName.Substring(0, Column.ColumnName.IndexOf("*", 0));
+                field.FieldType = Column.DataType.ToString();
+                int location = Column.ColumnName.Length - Column.ColumnName.IndexOf("*", 0) - 1;
+                field.FieldLength = Int32.Parse(Column.ColumnName.Substring(Column.ColumnName.IndexOf("*", 0) + 1, location));
+                DSFields.Add(field);
+            }
+
+            foreach (Field field in DSFields)
+            {
+                if (field.FieldType == "System.String")
+                {
+                    dbf.Fields.Add(new DbfField(field.FieldName, DbfFieldType.Character, Convert.ToByte(field.FieldLength)));
+                }
+                else if (field.FieldType == "System.Int32")
+                {
+                    dbf.Fields.Add(new DbfField(field.FieldName, DbfFieldType.Numeric, Convert.ToByte(field.FieldLength)));
+                }
+                else if (field.FieldType == "System.DateTime")
+                {
+                    dbf.Fields.Add(new DbfField(field.FieldName, DbfFieldType.Date, 8));
+                }
+                else
+                {
+                    dbf.Fields.Add(new DbfField(field.FieldName, DbfFieldType.Character, Convert.ToByte(field.FieldLength)));
+                }
+            }
+            foreach (string element in uniqueList)
+            {
+                var query = from r in dt.AsEnumerable()
+                            where r.Field<string>(campo2) == element
+                            select r;
+                DataTable dtDestino = query.CopyToDataTable<DataRow>();
+
+                DbfRecord record;
+                foreach (DataRow row in dtDestino.Rows)
+                {
+                    record = dbf.CreateRecord();
+                    for (int i = 0; i < dtDestino.Columns.Count; i++)
+                    {
+                        if (row[i].GetType().ToString() == "System.String")
+                        {
+                            byte[] utfBytes = utf8.GetBytes(row[i].ToString());
+                            byte[] isoBytes = Encoding.Convert(utf8, iso, utfBytes);
+                            string msg = iso.GetString(isoBytes);
+                            record.Data[i] = msg;
+                        }
+                        else record.Data[i] = row[i];
+                    }
+
+                }
+                dbf.Write(fi.DirectoryName + "//" + textBox3.Text + element.Substring(0, 3) + ".dbf", DbfVersion.FoxBaseDBase3NoMemo);
+                dbf.Records.Clear();
+
+            }
+
+            MessageBox.Show("Proceso concluido exitosamente");
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                procesaFechas();
+            }
+            else
+            {
+                procesaOtros();
+            }
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             this.textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                this.comboBox1.Enabled = false;
+                this.label3.Enabled = false;
+                this.label2.Text = "Campo Otro:";
+            }
+            else
+            {
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                this.comboBox1.Enabled = true;
+                this.label3.Enabled = true;
+                this.label2.Text = "Campo Fecha:";
+            }
+            else
+            {
+            }
         }
        
     }
